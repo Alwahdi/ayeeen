4d0c23cb9cb45b0f8159005535c6401987588970
Abdullah ALwahdi <abdullahalwahdi464@gmail.com>
2026-03-30 02:56:43 +0300

    test eas update

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -26,6 +26,6 @@ export const EditScreenInfo: React.FC<EditScreenInfoProps> = ({ path }) => {
 const styles = {
   codeHighlightContainer: `rounded-md px-1`,
   getStartedContainer: `items-center mx-12`,
-  getStartedText: `text-lg leading-6 text-center`,
+  getStartedText: `text-lg leading-6 text-center text-red-700`,
   homeScreenFilename: `my-2`,
 };
