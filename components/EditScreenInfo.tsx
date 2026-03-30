d32277e35c52b16f8d4da91af4d1bc05e7820147
Abdullah ALwahdi <abdullahalwahdi464@gmail.com>
2026-03-30 03:04:59 +0300

    fix the eas update channels

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -26,6 +26,6 @@ export const EditScreenInfo: React.FC<EditScreenInfoProps> = ({ path }) => {
 const styles = {
   codeHighlightContainer: `rounded-md px-1`,
   getStartedContainer: `items-center mx-12`,
-  getStartedText: `text-lg leading-6 text-center text-green-700`,
+  getStartedText: `text-lg leading-6 text-center text-blue-700`,
   homeScreenFilename: `my-2`,
 };
